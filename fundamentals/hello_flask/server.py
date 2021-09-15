1cbe5467c22507c25c7d68b080657c0e199e92d3
Max Barr <mxbarr@gmail.com>
2021-09-15 15:08:53 -0700

    2nd commit

--- fundamentals/hello_flask/server.py
+++ fundamentals/hello_flask/server.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -24,6 +24,15 @@ def show_user_profile(username, id):
     print(id)
     return "username: " + username + ", id: " + id
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+@app.route('/lists')
+def render_lists():
+    student_info= [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students= student_info)
 
+if __name__=="__main__":
+    app.run(debug=True)
