553a5d246057f8432a259c87cd5a45b9e361842d
Max Barr <mxbarr@gmail.com>
2022-07-11 14:29:39 -0700

    Practice with forms and session

--- flask_practice/form_and_results/server.py
+++ flask_practice/form_and_results/server.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 app.secret_key = "What's innn a name"
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def store_info():
+    print("Got post info")
+    print(request.form)
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['food'] = request.form['food']
+    session['comment'] = request.form['comment']
+    return redirect('/results')
+
+@app.route('/results')
+def show_results():
+    return render_template('results.html')
 
 
 if __name__ == "__main__":
